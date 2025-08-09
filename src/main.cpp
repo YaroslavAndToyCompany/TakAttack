c667b5f7fe0a818f44fc2046d0778d33ff086c04
toysmbb <tvinktoya@gmail.com>
2025-08-09 14:47:16 +0300

    feat: add new line

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <entt/entt.hpp>
-
+#include <iostream>
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 
@@ -8,14 +8,14 @@ int main() {
     if (!texture.loadFromFile("../res/assets/awesomeface.png"))
         return EXIT_FAILURE;
     sf::Sprite sprite(texture);
-
+    std::cout << "TAK ATTACK";
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        
         window.clear();
 
         window.draw(sprite);
