b31533d018c3df4eea924a1766b66dea6b35a4ec
toysmbb <tvinktoya@gmail.com>
2025-08-09 14:28:54 +0300

    Initital commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <entt/entt.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("../res/assets/awesomeface.png"))
+        return EXIT_FAILURE;
+    sf::Sprite sprite(texture);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        window.draw(sprite);
+
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
