99e1489f4a2983da0b1fe67dc43c212c75b4a0eb
YaroslavRomanenko <yaroslavromanenko456@gmail.com>
2025-08-10 18:34:09 +0300

    feat: add ResourceManager

---
+++ include/ResourceManager.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <map>
+
+class ResourceManger {
+public:
+    static void UpLoadTexture(const std::string& textureName, const std::string& texturePath);
+    static sf::Texture* GetTexture(const std::string& textureName);
+
+    static void ReleaseResources();
+
+private:
+    ResourceManger() { }
+
+    static std::map<std::string, sf::Texture*> s_resources;
+};
---
+++ src/ResourceManager.cpp
@@ -0,0 +1,32 @@
+#include "ResourceManager.h"
+
+#include <iostream>
+
+
+std::map<std::string, sf::Texture*> ResourceManger::s_resources;
+
+void ResourceManger::UpLoadTexture(const std::string& textureName, const std::string& texturePath)
+{
+    sf::Texture* texture = new sf::Texture();
+    texture->loadFromFile(texturePath);
+
+    std::pair<std::string, sf::Texture*> texturePackage(textureName, texture);
+    s_resources.insert(texturePackage);
+}
+
+sf::Texture* ResourceManger::GetTexture(const std::string& textureName)
+{
+    auto itr = s_resources.find(textureName);
+    if (itr == s_resources.end()) {
+        std::cerr << "Cannot retrieve " << textureName << " texture" << std::endl; 
+    }
+
+    return itr->second;
+}
+
+void ResourceManger::ReleaseResources()
+{
+    for (auto& texture : s_resources) {
+        delete texture.second;
+    }
+}
