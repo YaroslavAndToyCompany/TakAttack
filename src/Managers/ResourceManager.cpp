e5381e1ac44b08727acd6f1ca4211d63ee1a76ed
toysmbb <tvinktoya@gmail.com>
2025-10-26 11:17:55 +0200

    Update ResourceManager.cpp

--- src/Managers/ResourceManager.cpp
+++ src/Managers/ResourceManager.cpp
@@ -3,12 +3,12 @@
 ResourceManager::ResourceManager() {
 // TEMP ifdef
 #if defined(SFML_SYSTEM_WINDOWS)
-    UploadResource<sf::Texture>("Map1", "E:/projects/TakAttack/res/Maps/Map1.png");
-    UploadResource<sf::Texture>("MenuButton", "E:/projects/TakAttack/res/UI/Button.png");
-    UploadResource<sf::Texture>("MenuFrame", "E:/projects/TakAttack/res/UI/MenuFrame.png");
-    UploadResource<sf::Texture>("Castle", "E:/projects/TakAttack/res/Sprites/Castle.png");
-    UploadResource<sf::Texture>("Artillery", "E:/projects/TakAttack/res/Sprites/artillery.png");
-    UploadResource<sf::Font>("BoldPixels", "E:/projects/TakAttack/res/UI/Fonts/BoldPixels.ttf");
+    UploadResource<sf::Texture>("Map1", "../res/Maps/Map1.png");
+    UploadResource<sf::Texture>("MenuButton", "../bin/res/UI/Button.png");
+    UploadResource<sf::Texture>("MenuFrame", "../bin/res/UI/MenuFrame.png");
+    UploadResource<sf::Texture>("Castle", "../bin/res/Sprites/Castle.png");
+    UploadResource<sf::Texture>("Artillery", "../bin/res/Sprites/artillery.png");
+    UploadResource<sf::Font>("BoldPixels", "../bin/res/UI/Fonts/BoldPixels.ttf");
 #elif defined(SFML_SYSTEM_LINUX)
     UploadResource<sf::Texture>("Map1", "res/Maps/Map1.png");
     UploadResource<sf::Texture>("MenuButton", "res/UI/Button.png");
@@ -17,4 +17,4 @@ ResourceManager::ResourceManager() {
     UploadResource<sf::Texture>("Artillery", "res/Sprites/artillery.png");
     UploadResource<sf::Font>("BoldPixels", "res/UI/Fonts/BoldPixels.ttf");
 #endif
-}
+} 
