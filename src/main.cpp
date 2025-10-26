341f3e2c100f5ed2495c0e3f9c6935c5c2a6458b
toysmbb <tvinktoya@gmail.com>
2025-10-26 11:17:51 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "Game/Engine.hpp"
+#include <filesystem>
 
 int main() {
-   
+    std::cout << "Current path is " << std::filesystem::current_path()
+        << std::endl;
     Engine engine;
    
+    
+
     while (!engine.GetWindowPtr()->IsDone()) {
         engine.HandleInput();
         engine.ProcessEvents();
